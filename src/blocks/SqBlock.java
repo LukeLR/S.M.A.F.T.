a91f26d104f88fc10c0a9b71ddc9e802b08b14b1
Lukas Rose <lukas.rose@gmx.de>
2015-06-25 07:14:25 +0200

    Implemented Square-Block

---
+++ src/blocks/SqBlock.java
@@ -0,0 +1,23 @@
+package blocks;
+
+import java.awt.Color;
+
+public class SqBlock extends Block{
+	
+	
+	public SqBlock(int x, int y){
+		super(x,y);
+	}
+	
+	public SqBlock(int x, int y, Color color){
+		super(x,y,color);
+	}
+	
+	private void addPixels(){
+		pixels = new Pixel[4];
+		pixels[0] = new Pixel(0,0);
+		pixels[1] = new Pixel(1,0);
+		pixels[2] = new Pixel(0,1);
+		pixels[3] = new Pixel(1,1);
+	}
+}
