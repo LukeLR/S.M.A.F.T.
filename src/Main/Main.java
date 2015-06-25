fb4a266858aca3f935b4bc8f0110218d90b6e470
Lukas Rose <lukas.rose@gmx.de>
2015-06-25 13:55:06 +0200

    GameEngine

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,9 +9,23 @@ import ui.TestWindow;
 import blocks.Block;
 
 public class Main{
+	Color[][] statBlocks = new Color[50][50];
 	
 	public static void main(String[] args){
-		new GameEngine();
+		Main m = new Main();
+		TestWindow t = new TestWindow();
+		new GameEngine(m, t);
 	}
 	
+	public Color[][] getStats(){
+		return statBlocks;
+	}
+	
+	public Color getColor(int x, int y){
+		return statBlocks[x][y];
+	}
+	
+	public void setColor(int x, int y, Color c){
+		statBlocks[x][y] = c;
+	}
 }
