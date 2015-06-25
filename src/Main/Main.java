a11106e26a5607d34eefe96a5184c57591964488
Lukas Rose <lukas.rose@gmx.de>
2015-06-25 08:34:48 +0200

    Intersection Validation added to newBlock

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,13 @@
 package Main;
 
+import blocks.*;
+import java.awt.Color;
 import ui.TestWindow;
 import blocks.Block;
 
 public class Main {
+	Color[][] statBlocks = new Color[50][50];
+	
 	public static void main(String[] args){
 		new Main();
 	}
@@ -15,18 +19,40 @@ public class Main {
 	public void newGame(){
 		Block b = null;
 		
+		//die zentrale while-Schleife
 		do {
 		b = newBlock();
-		} while(moveDown(b));
+		} while(b != null && moveDown(b)); //b==null => neuer Block kann nicht mehr generiert werden
 	}
 	
 	private Block newBlock() {
-		// TODO Auto-generated method stub
-		return null;
+		//Zufälligen Block auswählen
+		Block[] blocks = new Block[]{
+			new IBlock(25, 0, Color.RED),
+			new LBlock(25, 0, Color.RED),
+			new SBlock(25, 0, Color.RED), 
+			new SqBlock(25, 0, Color.RED), 
+			new TBlock(25, 0, Color.RED), 
+			new ZBlock(25, 0, Color.RED)
+		};
+
+		Block result = blocks[(int)Math.random()*6];
+		
+		//überprüfen ob der block sich dem Spielfeld hinzufügen lässt
+		Position[] pp = b.getBlockedPosition();
+		if(pp!=null) for(Position p : pp){
+			if(statBlocks[p.getX()][p.getY()] != null) return null
+		}
+		
+		return result;
 	}
 
 	private boolean moveDown(Block b) {
-		// TODO Auto-generated method stub
-		return false;
+		b.setY(b.getY() + 1);
+		
+		//überprüfen ob der Block noch passt
+
+		
+		return true;
 	}
 }
