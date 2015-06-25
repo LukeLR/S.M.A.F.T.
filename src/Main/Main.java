11c274225b1a7c339016c30abf43ff4a29bf2f44
Lukas Rose <lukas.rose@gmx.de>
2015-06-25 07:35:19 +0200

    Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,29 @@
 package Main;
 
+import blocks.Block;
+
 public class Main {
 	public static void main(String[] args){
-		new Main().newGame();
-	}
-	
-	public Main(){
 		new Window();
 	}
+	
+//	public Main(){
+//		new Window();
+//	}
+//	
+//	public void newGame(){
+//		do {
+//		Block b = newBlock();
+//		} while(moveDown(b));
+//	}
+//private Block newBlock() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//}
+//
+//	private boolean moveDown(Block b) {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
 }
