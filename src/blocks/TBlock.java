89be6f58ca7eb39409a6e53a65d3486c8a722bf3
Lukas Rose <lukas.rose@gmx.de>
2015-06-25 07:11:55 +0200

    Implemented T-Block

---
+++ src/blocks/TBlock.java
@@ -0,0 +1,24 @@
+package blocks;
+
+import java.awt.Color;
+
+public class TBlock extends Block {
+	
+	
+	public TBlock(int x, int y){
+		super(x,y);
+	}
+	
+	public TBlock(int x, int y, Color color){
+		super(x,y,color);
+	}
+	
+	private void addPixels(){
+		pixels = new Pixel[5];
+		pixels[0] = new Pixel(0,0);
+		pixels[1] = new Pixel(1,0);
+		pixels[2] = new Pixel(2,0);
+		pixels[3] = new Pixel(1,1);
+		pixels[4] = new Pixel(1,2);
+	}
+}
